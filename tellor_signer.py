9d96991ce5243b7234bc96d186b86bb56e18208b
themandalore <fett.nicholas@yahoo.com>
2020-12-08 14:21:02 -0500

    not working

--- tellor_signer.py
+++ tellor_signer.py
@@ -1,15 +1,18 @@
 
 from stark_cli import sign_cli,hash_price,public_cli
-import time,requests,json,hashlib
+import time,requests,json,hashlib,os
 from web3 import Web3
 from eth_account.messages import encode_defunct
 from web3.auto import w3
 from bitstring import BitArray
 from signature import pedersen_hash
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-
-privateKey =
+privateKey = os.getenv("PRIVATEKEY")
+myName = "TRB"
 submissionURL = "http://api.stage.dydx.exchange/v3/price"
 btcAPIs = ["json(https://api.pro.coinbase.com/products/BTC-USD/ticker).price",
 		"json(https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd).bitcoin.usd"]
@@ -17,7 +20,6 @@ ethAPIs = [		"json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price",
 		"json(https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd).ethereum.usd"]
 
 btc = {
-  "oracle": "TRB",
   "price":0,
   "asset":"BTC/USD",
   "timestamp": 0,
@@ -25,12 +27,21 @@ btc = {
   "timeLastPushed":0
 }
 eth = {
-  "oracle": "TRB",
   "price": 0,
   "asset":"ETH/USD",
   "timestamp": 0,
   "lastPushedPrice":0,
   "timeLastPushed":0
+}
+
+submitData = {
+	"starkKey":0,
+	"timestamp":0,
+	"price":0,
+	"assetName":0,
+	"oracleName":0,
+	"signatureR":0,
+	"signatureS":0
 }
 
 def getAPIValues():
@@ -81,20 +92,22 @@ def signValue(data):
 	starkKey = EthKeyToStarkKey(privateKey)
 	intKey = int(starkKey,16)
 	intData = int(data,16)
-	return sign_cli(intKey,intData)
+	x = sign_cli(intKey,intData)
+	y = x.split(' ')
+	submitData["signatureR"] = y[0]
+	submitData["signatureS"] = y[1]
 
-def formatData(d
-	n = data["oracle"].enata):
-	print("Current Prices ",data["asset"]," : ",data["price"]/(10**18))code('utf-8').hex()
-	name =int(n,16)
+def formatData(data):
 	n = Web3.toHex(str.encode(data["asset"]))
 	c = bin(int(data["asset"].encode('utf-8').hex(),16))[:128]
 	asset = hex(int(c, 2)).ljust(34,"0")
 	asset =int(asset,16)
+	name = int(myName.encode('utf-8').hex(),16)
 	return hash_price(name,asset,data["price"],data["timestamp"])
 
 def submitSignature(_signedData):
-	x = requests.post(submissionURL, data = _signedData)
+	print(_signedData)
+	x = requests.post(submissionURL,data=_signedData)
 	print(x)
 
 def EthKeyToStarkKey(eth_key):
@@ -106,15 +119,20 @@ def EthKeyToStarkKey(eth_key):
 	return stark_private_key
 
 def TellorSignerMain():
+	submitData["starkKey"] = public_cli(int(EthKeyToStarkKey(privateKey),16))
+	submitData["oracleName"] = myName
 	while True:
 		assets = getAPIValues()
 		for i in range(len(assets)):
 			apiData = assets[i]
 			data = formatData(apiData)
-			signedData = signValue(data)
-			if assets[i]["timestamp"]- assets[i]["timeLastPushed"] > 300 or abs(assets[i]["price"] - assets[i]["lastPushedPrice"]) > .01:
-				print("submitting data :", signedData)
-				submitSignature(signedData)
+			signValue(data)
+			if assets[i]["timestamp"]- assets[i]["timeLastPushed"] > 300 or abs(assets[i]["price"] - assets[i]["lastPushedPrice"]) > .02:
+				submitData["assetName"] = assets[i]["asset"]
+				submitData["price"] = assets[i]["price"]
+				submitData["timestamp"] = assets[i]["timestamp"]
+				print("submitting data :", submitData)
+				submitSignature(submitData)
 				assets[i]["lastPushedPrice"] = assets[i]["price"]
 				assets[i]["timeLastPushed"] = assets[i]["timestamp"]
 		time.sleep(10);
@@ -125,4 +143,12 @@ TellorSignerMain()
 #print(medianize(ethAPIs))
 #print(public_cli(int(privateKey,16)))
 
-#print(public_cli(int(EthKeyToStarkKey(privateKey),16))
+#print(public_cli(int(EthKeyToStarkKey(privateKey),16)))
+
+#Add   bitstamp
+  # bittrex
+  # gemini
+  # kraken
+
+#curl -X POST --data '{"starkKey": "0x13ebb76f3d0c31448a84bcfca6edc246637f3f6d8aa5ab2cb7e030d5b7c9034", "timestamp": 1607454394, "price": 18839000000000000000000, "assetName":"BTC-USD", "oracleName":"Tellor", "signatureR": "0x26eba72c3328edc5f4897979bff80a1d8325c6acf1823c6fa5be21804d9ba67", "signatureS": "0x3e43b9d54e626f8d17bb24d3b89b1b00e43b192ec844a1561d74f78bf2b35e0"}' https://api.stage.dydx.exchange/v3/price
+
