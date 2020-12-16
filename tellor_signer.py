96770e096ab2e40a75a7cbafa9ce791ee651ae9b
Tally Wiesenberg <t.j.wies@gmail.com>
2020-12-16 14:20:16 -0500

    docstrings and medianizing

--- tellor_signer.py
+++ tellor_signer.py
@@ -7,25 +7,6 @@ from web3.auto import w3
 from bitstring import BitArray
 from signature import pedersen_hash
 from dotenv import load_dotenv
-# import logging
-
-# # These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# # You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# # The only thing missing will be the response.body which is not logged.
-# try:
-#     import http.client as http_client
-# except ImportError:
-#     # Python 2
-#     import httplib as http_client
-# http_client.HTTPConnection.debuglevel = 1
-
-# # You must initialize logging, otherwise you'll not see debug output.
-# logging.basicConfig()
-# logging.getLogger().setLevel(logging.DEBUG)
-# requests_log = logging.getLogger("requests.packages.urllib3")
-# requests_log.setLevel(logging.DEBUG)
-# requests_log.propagate = True
-
 
 load_dotenv()
 
@@ -33,16 +14,11 @@ load_dotenv()
 privateKey = os.getenv("PRIVATEKEY")
 myName = "Tellor"
 submissionURL = "https://api.stage.dydx.exchange/v3/price"
-btcAPIs = [
-	"json(https://api.pro.coinbase.com/products/BTC-USD/ticker).price",
-	"json(https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd).bitcoin.usd"
-	"json(https://api.bittrex.com/api/v1.1/public/getticker?market=BTC-USD).result.last"]
-ethAPIs = [
-	"json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price",
-	"json(https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd).ethereum.usd",
-	"json(https://api.bittrex.com/api/v1.1/public/getticker?market=ETC-USD).result.last"]
 
-btcAPIs_v2 = [
+# BTC and ETH api endpoints from centralized exchanges
+# Each endpoint is encased in a list with the keywords that parse the JSON to the last price
+
+btcAPIs = [
 	["https://api.pro.coinbase.com/products/BTC-USD/ticker", "price"],
 	["https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd", "bitcoin", "usd"],
 	["https://api.bittrex.com/api/v1.1/public/getticker?market=USD-BTC", 'result', 'Last'],
@@ -51,13 +27,16 @@ btcAPIs_v2 = [
 
 ]
 
-ethAPIs_v2 = [
+ethAPIs = [
 	["https://api.pro.coinbase.com/products/ETH-USD/ticker", "price"],
 	["https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd", "ethereum", "usd"],
 	["https://api.bittrex.com/api/v1.1/public/getticker?market=USD-ETH", 'result', 'Last'],
 	["https://api.gemini.com/v1/pubticker/ethusd", 'last'],
 	["https://api.kraken.com/0/public/Ticker?pair=ETHUSDC", 'result', "ETHUSDC", 'c', 0]
 ]
+
+# btc, eth are helper dictionaries used to distinguish btc prices and eth prices
+# these are used for data wrangling, from centralized API endpoints to medianization
 
 btc = {
   "price":0,
@@ -76,71 +55,54 @@ eth = {
   "timeLastPushed":0
 }
 
+#final data submission format
+
 submitData = {
 	"starkKey":0,
 	"timestamp":0,
 	"price":0,
-	"assetName":0,
+	"assetName":0, #btc or eth
 	"oracleName":0,
 	"signatureR":0,
 	"signatureS":0
 }
 
 def getAPIValues():
+
 	btc["timestamp"] = int(time.time())
-	price = medianize(btcAPIs_v2)
+	price = medianize(btcAPIs)
 	btc["strPrice"] = str(price)
 	btc["price"] = int(price*(10**18))
 	eth["timestamp"] = int(time.time())
-	price = medianize(ethAPIs_v2)
+	price = medianize(ethAPIs)
 	eth["strPrice"] =str(price)
 	eth["price"] = int(price*(10**18))
 	return [btc,eth]
 
 def medianize(_apis):
+	'''
+	Medianizes price of an asset from a selection of centralized price APIs
+	'''
 	finalRes = []
 	didGet = False
 	n = 0
 	for i in _apis:
-		_res = fetchAPI_v2(i)
+		_res = fetchAPI(i)
+		if not _res:
+			pass
 		if _res > 0:
 			didGet = True
 			finalRes.append(_res)
 	
 	#sort final results
 	finalRes.sort()
-	return finalRes[int(len(finalRes)//2)]
+	return finalRes[int(len(finalRes)/2)]
 
-def fetchAPI(_api):
-	_api = _api.replace("'", "")
-	json = _api.split('(')[0]
-	if('json' in json):
-		_api = _api.split('(')[1]
-		filter = _api.split(').')[1]
-	_api = _api.split(')')[0]
-	try:
-		response = requests.request("GET", _api)
-	except:
-		response = 0
-		print('API ERROR',_api)
-	if('json' in json):
-		if(len(filter)):
-			numFils = filter.count('.') + 1
-			price = response.json()
-			for i in range(numFils):
-				thisFilter =  filter.split('.')[i]
-				price =price[thisFilter]
-		else:
-			price = response.json()
-	else:
-		price = response
-	return float(price)
-
-
-def fetchAPI_v2(public_api):
+def fetchAPI(public_api):
 	''' 
-	Return ticker price from public exchange web APIs
-	Input: list of str -- public api endpoint with any necessary json parsing keywords
+	Fetches price data from centralized public web API endpoints
+	Returns: (str) ticker price from public exchange web APIs
+	Input: (list of str) public api endpoint with any necessary json parsing keywords
 	'''
 	try:
 		#Parse list input
@@ -218,21 +180,4 @@ def testSubmit():
 	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 	print(requests.post(submissionURL,data=json.dumps(data),headers=headers))
 
-#testSubmit()
 TellorSignerMain()
-#print(medianize(btcAPIs))
-#print(medianize(ethAPIs))
-#print(public_cli(int(privateKey,16)))
-
-#print(public_cli(int(EthKeyToStarkKey(privateKey),16)))
-
-#Add   bitstamp
-  # bittrex
-  # TODO gemini
-  # TODO kraken
-
-if __name__ == '__main__':
-	for i in btcAPIs_v2:
-		print(i[0])
-		print('---')
-		print(fetchAPI_v2(i))
